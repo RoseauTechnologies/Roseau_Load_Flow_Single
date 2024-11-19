92c05b8ef0f279d5b0459bdfc2e4743e63c97f0e
Saelyos <seb.vallet@gmail.com>
2024-11-19 10:41:35 +0100

    Add flexible parameters

--- roseau/load_flow_single/__init__.py
+++ roseau/load_flow_single/__init__.py
@@ -2,7 +2,7 @@ from roseau.load_flow_single.models.branches import AbstractBranch
 from roseau.load_flow_single.models.buses import Bus
 from roseau.load_flow_single.models.core import Element
 from roseau.load_flow_single.models.lines import Line, LineParameters
-from roseau.load_flow_single.models.loads import AbstractLoad, CurrentLoad, FlexibleParameter, ImpedanceLoad, PowerLoad
+from roseau.load_flow_single.models.loads import AbstractLoad, CurrentLoad, FlexibleParameter, ImpedanceLoad, PowerLoad, Projection, Control
 from roseau.load_flow_single.models.sources import VoltageSource
 from roseau.load_flow_single.models.switches import Switch
 from roseau.load_flow_single.models.transformers import Transformer, TransformerParameters
@@ -23,5 +23,7 @@ __all__ = [
     "Transformer",
     "TransformerParameters",
     "FlexibleParameter",
+    "Projection",
+    "Control",
     "AbstractBranch",
 ]
--- roseau/load_flow_single/models/loads/__init__.py
+++ roseau/load_flow_single/models/loads/__init__.py
@@ -1,4 +1,5 @@
-from roseau.load_flow.models.loads.flexible_parameters import FlexibleParameter
+from roseau.load_flow.models.loads.flexible_parameters import Projection
+from roseau.load_flow_single.models.loads.flexible_parameters import Control, FlexibleParameter
 from roseau.load_flow_single.models.loads.loads import AbstractLoad, CurrentLoad, ImpedanceLoad, PowerLoad
 
-__all__ = ["PowerLoad", "AbstractLoad", "FlexibleParameter", "CurrentLoad", "ImpedanceLoad"]
+__all__ = ["PowerLoad", "AbstractLoad", "FlexibleParameter", "Control", "Projection", "CurrentLoad", "ImpedanceLoad"]
